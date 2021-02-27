f28f0858d7dbb5e71e007490c8bcae9c190414ee
WahAkshat <jeet11chatterjee@gmail.com>
2021-02-27 13:08:22 +0530

    Conditional Login

--- app/src/main/java/com/example/cuenta/MainActivity.java
+++ app/src/main/java/com/example/cuenta/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.cuenta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button b1;
+    private Button b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.userLog);
+        b2 = (Button) findViewById(R.id.orgLog);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, UserLogin.class);
+                Toast.makeText(MainActivity.this,"User Login",Toast.LENGTH_LONG).show();
+                startActivity(i);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, OrgLogin.class);
+                Toast.makeText(MainActivity.this,"Org Login",Toast.LENGTH_LONG).show();
+                startActivity(i);
+            }
+        });
     }
 }
