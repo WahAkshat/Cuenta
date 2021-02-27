9ca7d658844550447c7922b0c07d3def0f4c6fde
abhishek1052 <68838873+abhishek1052@users.noreply.github.com>
2021-02-27 19:40:13 +0530

    finished qr scanner

--- app/src/main/java/com/example/cuentaIt/qr_scanner.java
+++ app/src/main/java/com/example/cuentaIt/qr_scanner.java
@@ -3,18 +3,55 @@ package com.example.cuentaIt;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
 import com.budiyev.android.codescanner.DecodeCallback;
 import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class qr_scanner extends AppCompatActivity {
     CodeScanner codescanner;
     CodeScannerView scannerview;
     TextView resultData;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        requestforcamera();
+
+
+
+    }
+    private void requestforcamera(){
+        Dexter.withActivity(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                codescanner.startPreview();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Toast.makeText(qr_scanner.this, "CAMERA PERMISIION IS REQUIRED", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +70,12 @@ public class qr_scanner extends AppCompatActivity {
                  });
              }
          });
-
+scannerview.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        codescanner.startPreview();
+    }
+});
 
     }
 }
