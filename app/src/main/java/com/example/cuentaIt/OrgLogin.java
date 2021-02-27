bd12319d02065efd7dd2566d33e97cff78af62ab
abhishek1052 <68838873+abhishek1052@users.noreply.github.com>
2021-02-27 15:21:02 +0530

    made QR GENERATOR PAGE

--- app/src/main/java/com/example/cuentaIt/OrgLogin.java
+++ app/src/main/java/com/example/cuentaIt/OrgLogin.java
@@ -2,7 +2,11 @@ package com.example.cuentaIt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class OrgLogin extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class OrgLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_org_login);
+        Button toqr=findViewById(R.id.toqr);
+        toqr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(OrgLogin.this, qr_generator.class);
+                Toast.makeText(OrgLogin.this,"GENERATE QR",Toast.LENGTH_LONG).show();
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cuentaIt/qr_generator.java
+++ app/src/main/java/com/example/cuentaIt/qr_generator.java
@@ -2,13 +2,46 @@ package com.example.cuentaIt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 public class qr_generator extends AppCompatActivity {
-
+    EditText qrvalue;
+    Button generatebutton,scanbutton;
+    ImageView qrImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr_generator);
+        qrvalue=findViewById(R.id.qrinput);
+        generatebutton=findViewById(R.id.generatebutton);
+        scanbutton=findViewById(R.id.scanbutton);
+        qrImage=findViewById(R.id.qrimage);
+
+        generatebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data=qrvalue.getText().toString();
+                if(data.isEmpty()){
+                    qrvalue.setError("Value is required");
+                }
+                else {
+                    QRGEncoder qrgEncoder = new QRGEncoder(data, null, QRGContents.Type.TEXT, 500);
+                    try {
+                        Bitmap qrBits = qrgEncoder.getBitmap();
+                        qrImage.setImageBitmap(qrBits);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }    }
+        });
     }
 }
